9c9101c2b86584d2028746adea304688b8781065
SierraSon97 <swj1265@naver.com>
2022-04-10 11:13:35 +0900

    Initial commit

---
+++ src/main/java/com/example/restfulapi_study/RestfulApiStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulapi_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulApiStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restfulapi_study/RestfulApiStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulapi_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
